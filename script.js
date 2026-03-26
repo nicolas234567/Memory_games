082b6bbd6dd84a022928dfa270e7b50346d7317d
Nicolas Bregevin <nicolasbregevin878@gmail.com>
2026-03-26 17:59:07 +0100

    ajout du système de point

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const originalValues = [0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7];
 values = [...originalValues];
+let moveCount = 0;
+let nombrePaire = 0;
+let bestMoveCount = 100;
 
 function startGame(){
     const startButton = document.getElementById("startButton");
     shuffle(values);
+    moveCount = 0;
+    document.getElementById("moveCount").textContent = `${moveCount}`;
+    nombrePaire = 0;
+    document.getElementById("nombrePaire").textContent = `${nombrePaire}`;
     gameBoard();
     hideCards();
 }
@@ -34,6 +41,7 @@ function gameBoard(){
         card.classList.add("card");
         card.addEventListener("click", function(){
             this.classList.add("revealed");
+            checkCards();
          });
 
         const value = values[i];
@@ -43,5 +51,58 @@ function gameBoard(){
         card.textContent = value;
 
         board.appendChild(card);
+    }
+}
+
+//verifier les cartes
+function checkCards(){
+    const revealedCards = document.querySelectorAll(".card.revealed");
+    if(revealedCards.length <= 2){
+        updateMoveCount();
+        if(revealedCards[0].dataset.value === revealedCards[1].dataset.value){
+            revealedCards[0].classList.add("defrevealed");
+            revealedCards[1].classList.add("defrevealed");
+            nombrePaire++;
+            document.getElementById("nombrePaire").textContent = `${nombrePaire}`;
+            checkWin();
+            removeCards();
+        }
+        else{ 
+            setTimeout(() => {
+                removeCards();
+            }, 2000);    
+        }
+    }
+    else{
+        return;
+    }
+}
+
+// enlever les cartes
+function removeCards(){
+    const revealedCards = document.querySelectorAll(".card.revealed");
+    revealedCards[0].classList.remove("revealed");
+    revealedCards[1].classList.remove("revealed");
+}
+// compter le nombre de coups
+function updateMoveCount(){
+    moveCount++;
+    document.getElementById("moveCount").textContent = `${moveCount}`;
+}
+
+// verifier si le jeu est terminé
+function checkWin(){
+    if(nombrePaire === 8){setTimeout(() => {
+            alert(`Félicitations ! Vous avez gagné en ${moveCount} coups.`);
+            updateBestScore();
+        }, 1000);
+    }
+}
+
+// mettre a jour le meilleur score
+function updateBestScore(){
+    if(moveCount < bestMoveCount){
+        bestMoveCount = moveCount;
+        document.getElementById("bestMoveCount").textContent = `${bestMoveCount}`;
     }
 }
